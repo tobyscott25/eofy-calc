36ee65a5c02722d01da6b26f09204962a191015c
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-07-17 08:10:18 +1000

    Calculate tax paid

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 
-annual_salary = float(input("Your annual salary? ($x,000): ")) * 1000
+annual_salary = float(input("Your annual salary? ($x,000 e.g. \"50\" = 50,000): ")) * 1000
 salary_sacrifice = float(input("What percentage, are you salary sacrificing before tax? (%): "))
 taxable_income = annual_salary * (1 - salary_sacrifice / 100)
+
+
+
+if taxable_income <= 18200:
+	# Tax-free threshold
+	tax = 0
+elif taxable_income >= 18201 and taxable_income <= 45000:
+	tax_rate = 0.19
+	# Tax every dollar after $18,200 at 19%
+	tax = 0 + (taxable_income - 18200) * tax_rate
+elif taxable_income >= 45001 and taxable_income <= 120000:
+	tax_rate = 0.325
+	# Tax every dollar after $45,000 at 32.5%
+	tax = 5092 + (taxable_income - 45000) * tax_rate
+elif taxable_income >= 120001 and taxable_income <= 180000:
+	tax_rate = 0.37
+	# Tax every dollar after $120,000 at 37%
+	tax = 29467 + (taxable_income - 120000) * tax_rate
+else:
+	tax_rate = 0.45
+	# Tax every dollar after $180,000 at 45%
+	tax = 51667 + (taxable_income - 180000) * tax_rate
+
+remainder = taxable_income - tax
+
+
 
 def print_report():
 
@@ -11,6 +37,8 @@ def print_report():
 	print('\n')
 	print(f'Salary sacrifice (pre-tax): {salary_sacrifice}% (${annual_salary - taxable_income})')
 	print(f'Taxable income: ${taxable_income}')
+	print('\n')
+	print(f'You pay ${tax}, leaving you with ${remainder}')
 	print('============================================================')
 	print('\n')
 
