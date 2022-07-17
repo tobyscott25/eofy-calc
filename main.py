4f5db6f5ebf051ee83213ca28feebd1844b35422
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-07-17 10:03:47 +1000

    Rearrange tax logic

--- main.py
+++ main.py
@@ -17,31 +17,46 @@ luxuries = float(input("Estimated monthly cost on luxuries (e.g. streaming subsc
 
 taxable_income = annual_salary * (1 - salary_sacrifice / 100)
 
-if taxable_income <= 18200:
-	# Tax-free threshold
+if taxable_income <= 18200: # Up to $18,200
 	tax = 0
-elif taxable_income >= 18201 and taxable_income <= 45000:
-	tax_rate = 0.19
+
+elif taxable_income <= 45000: # Up to $45,000
+
 	# Tax every dollar after $18,200 at 19%
-	tax = (taxable_income - 18200) * tax_rate
+	tax_rate = 0.19
+	amount_to_tax = taxable_income - 18200
+	tax = amount_to_tax * tax_rate
+
 	# Add the tax from the previous bracket(s)
 	tax = tax + 0
-elif taxable_income >= 45001 and taxable_income <= 120000:
-	tax_rate = 0.325
+
+elif taxable_income <= 120000: # Up to $120,000
+
 	# Tax every dollar after $45,000 at 32.5%
-	tax = (taxable_income - 45000) * tax_rate
+	tax_rate = 0.325
+	amount_to_tax = taxable_income - 45000
+	tax = amount_to_tax * tax_rate
+
 	# Add the tax from the previous bracket(s)
 	tax = tax + 5092
-elif taxable_income >= 120001 and taxable_income <= 180000:
-	tax_rate = 0.37
+
+elif taxable_income <= 180000: # Up to $180,000
+
 	# Tax every dollar after $120,000 at 37%
-	tax = (taxable_income - 120000) * tax_rate
+	tax_rate = 0.37
+	amount_to_tax = taxable_income - 120000
+	tax = amount_to_tax * tax_rate
+
 	# Add the tax from the previous bracket(s)
 	tax = tax + 29467
-else:
-	tax_rate = 0.45
+
+else: # Over $180,000
+
 	# Tax every dollar after $180,000 at 45%
-	tax = (taxable_income - 180000) * tax_rate
+	tax_rate = 0.45
+	amount_to_tax = taxable_income - 180000
+	tax = amount_to_tax * tax_rate
+
 	# Add the tax from the previous bracket(s)
 	tax = tax + 51667
 
